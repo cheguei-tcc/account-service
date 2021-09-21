1b9622fc9223172b8d98138cfe18001a56fa5ac9
iamseki <chrisyuri_19@hotmail.com>
2021-09-20 23:45:29 -0300

    adding notification base domain

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//import { MongooseModule } from '@nestjs/mongoose';
 import { KnexModule } from 'nestjs-knex';
 import { AccountModule } from '../account/account.module';
+import { NotificationModule } from '../notification/notification.module';
 @Module({
   imports: [
     KnexModule.forRoot({
       config: require('../../knexfile'),
     }),
-    //MongooseModule.forRoot(process.env.MONGO_URI || 'localhost:2017/cheguei'),
     AccountModule,
+    NotificationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
